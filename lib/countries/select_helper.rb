38e6ed7c826a08d7d82484176a02477d17df2a28
goodwill <william@tofugear.com>
2012-06-05 11:00:31 +0800

    Still not html_safe

--- lib/countries/select_helper.rb
+++ lib/countries/select_helper.rb
@@ -16,7 +16,7 @@ module ActionView
           country_options += "<option value=\"\" disabled=\"disabled\">-------------</option>\n".html_safe
         end
 
-        return country_options + options_for_select(ISO3166::Country::Names, selected)
+        return (country_options + options_for_select(ISO3166::Country::Names, selected)).html_safe
       end
     end
 
